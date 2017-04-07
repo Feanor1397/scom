899f60a55cf3f6aa7f9ec62b38613eb1ede3185a
Elena <feanor1397@gmail.com>
2017-04-08 02:13:31 +0500

    Ncurses tui

---
+++ include/ui.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <cursesw.h>
+#include <cursesm.h>
+
+namespace scom
+{
+  class TextField
+  {
+    private:
+      NCursesPanel *border, *field;
+      char buff[1024];
+      int _height, _width;
+      int _y, _x;
+    public:
+      TextField(int height, int width, int y, int x);
+      virtual ~TextField();
+      const char* read();
+  };
+
+  class TextFrame
+  {
+    private:
+      NCursesPanel *border, *field;
+      int _height, _width;
+      int _y, _x;
+    public:
+      TextFrame(int height, int width, int y, int x);
+      virtual ~TextFrame();
+      void print(const char* message);
+  };
+
+  class TextDuplex
+  {
+    private:
+      TextField *field;
+      TextFrame *frame;
+      int _height, _width;
+      int _y, _x;
+      int _f_height;
+    public:
+      TextDuplex(int height, int width, int y, int x, int f_height);
+      virtual ~TextDuplex();
+      void print(const char* message);
+      const char* read();
+  };
+
+  class Menu{};
+}
---
+++ src/ui.cpp
@@ -0,0 +1,106 @@
+#include <ui.hpp>
+
+//
+//==========================================================================
+//Text Field for entering mesage
+//==========================================================================
+//
+
+scom::TextField::TextField(int height, int width, int y, int x)
+: _height(height),
+  _width(width),
+  _y(y),
+  _x(x)
+{
+  border = new NCursesPanel(_height, _width, _y, _x);
+  border->box();
+  field = new NCursesPanel(_height - 2, _width - 2, _y + 1, _x + 1);
+  field->scrollok(true);
+}
+
+scom::TextField::~TextField()
+{
+  delete field;
+  delete border;
+}
+
+const char* scom::TextField::read()
+{
+  const char* to_ret;
+  field->getstr(buff);
+  field->clear();
+  to_ret = buff;
+  return to_ret;
+}
+
+//
+//==========================================================================
+//Text Field for printing mesages
+//==========================================================================
+//
+
+scom::TextFrame::TextFrame(int height, int width, int y, int x)
+: _height(height),
+  _width(width),
+  _y(y),
+  _x(x)
+{
+  border = new NCursesPanel(_height, _width, _y, _x);
+  border->box();
+  field = new NCursesPanel(_height - 2, _width - 2, _y + 1, _x + 1);
+  field->scrollok(true);
+}
+
+scom::TextFrame::~TextFrame()
+{
+  delete field;
+  delete border;
+}
+
+void scom::TextFrame::print(const char* message)
+{
+  field->addstr(message);
+  field->addch('\n');
+  field->refresh();
+}
+
+//
+//==========================================================================
+//Both Fields inside one object
+//==========================================================================
+//
+
+scom::TextDuplex::TextDuplex(int height, int width,
+                             int y, int x, int f_height)
+: _height(height),
+  _width(width),
+  _y(y),
+  _x(x),
+  _f_height(f_height)
+{
+  frame = new scom::TextFrame(_height - _f_height,
+                              _width,
+                              _y,
+                              _x);
+
+  field =new scom::TextField(_f_height,
+                             _width,
+                             _y + _height - _f_height,
+                             _x);
+}
+
+scom::TextDuplex::~TextDuplex()
+{
+  delete field;
+  delete frame;
+}
+
+void scom::TextDuplex::print(const char* message)
+{
+  frame->print(message);
+}
+
+const char* scom::TextDuplex::read()
+{
+  return field->read();
+}
